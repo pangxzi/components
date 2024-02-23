74fc51e3bd918aa85f7c4f72a77d54e6adf72b39
mwj <214410781@qq.com>
2024-02-23 15:57:35 +0800

    📃 docs: Remove hero section from .dumirc.ts

--- .dumirc.ts
+++ .dumirc.ts
@@ -8,20 +8,6 @@ export default defineConfig({
     socialLinks: {
       github: homepage,
     },
-    hero: {
-      title: 'ProEditor',
-      description: '🌟 通用编辑器 UI 框架',
-      actions: [
-        {
-          text: '快速开始 →',
-          link: '/guide/intro',
-        },
-        {
-          text: 'Github',
-          link: 'https://github.com/ant-design/pro-editor',
-        },
-      ],
-    },
   },
   html2sketch: {},
   extraBabelPlugins: ['antd-style'],
